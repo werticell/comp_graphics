e488d49062db1bf88f5c13cd86a156978a930303
Mikhail <s.tdm@icloud.com>
2022-04-03 04:13:56 +0300

    better

--- Task1/bin/fragment_shader2.glsl
+++ Task1/bin/fragment_shader2.glsl
@@ -1,6 +1,6 @@
 #version 120
 
 void main() {
-    // Output color = Some kind of blue
-    gl_FragColor = vec4(0, 0, 0.8, 0.5);
+    // Output color = Some kind of Yellow
+    gl_FragColor = vec4(1, 0.9, 0.2, 0.7);
 }
