bdfe0ae5f69308263dfc4423bc13a6c1dddc7688
Mikhail <s.tdm@icloud.com>
2022-05-04 01:51:29 +0300

    better

--- Task4/text_manager.hpp
+++ Task4/text_manager.hpp
@@ -1,19 +1,21 @@
 #pragma once
 
+#include <text2D.hpp>
+
 class TextManager {
  public:
-  TextManager(const char * path) {
-    initText2D(path);
+  TextManager(const char* path, const char* text_vertex_shader,
+              const char* text_fragment_shader) {
+    support::InitText2D(path, text_vertex_shader, text_fragment_shader);
   }
 
   ~TextManager() {
     // Delete the text's VBO, the shader and the texture
-    cleanupText2D();
+    support::CleanupText2D();
   }
 
   void Display(uint64_t number) {
     std::string fireballs("Doggos fed: " + std::to_string(number));
-    printText2D(fireballs.data(), 0, 550, 25);
+    support::PrintText2D(fireballs.data(), 0, 550, 25);
   }
-
 };
